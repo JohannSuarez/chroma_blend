3cf4e5cfbff1902d849fcded44250baff7fab3fb
JohannSuarez <johann.suarez92@gmail.com>
2021-12-12 01:10:09 -0800

    Tackling concurrent chroma frame size adjustment.

--- cblend_modules/colorizer.py
+++ cblend_modules/colorizer.py
@@ -5,16 +5,17 @@ Then blended over the low-res black and white picture.
 '''
 
 import argparse
+import concurrent.futures
 import numpy as np
 from PIL import Image, ImageEnhance
 from skimage import color
+from typing import List
 
 
 class BColors:
     '''
     Color constants. This should be moved elsewhere.
     '''
-
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -30,25 +31,58 @@ class BColors:
 
 class Colorizer:
 
-    def __init__(self): # Initialize (Think: A constructor)
-        # Currently, there are no variables
-        # that are shared between methods. 
-        # No need for class-wide variables yet.
+    def __init__(self) -> None:
         pass
 
   
+    def size_check(self, frame_names: List[tuple] ) -> None:
+        '''
+        Check an image sample from both bw_frames and source_frames. 
+        We can do this by grabbing the first element of frame_names.
+
+        If the two image samples are the same size, use size_corrector 
+        on the entirety of either the bw_frames or the color_frames 
+        depending on which of them needs to be upsized.
+        '''
+        
+        print(frame_names[0][0][0])
+        print(frame_names[0][0][1])
+
+        bw_image = Image.open(frame_names[0][0][0]) # Black and white 
+        remastered_image = Image.open(frame_names[0][0][1]) # Colored
+
+        # Variables to perform size consistency check
+        bw_w, bw_h = bw_image.size
+        cl_w, cl_h = remastered_image.size
+
+        if(bw_w - cl_w != 0) or (bw_h - cl_h != 0):
+            print("Video pair are different sizes. Correcting frames..")
+
+            # It's here that you have to asynchronously resize either the 
+            # entirety of the bw frames or the colored frames.
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                # What do you need to pass into size_corrector?
+                # Well, what does the size_corrector do?
+                # Resize chroma, to make it as big as the luma.
+                # So what does size_corrector need?
+                # A list of all the chroma frames, and the proper height/width for them.
+
+                # Let chroma_obj be a dictionary that contains a list
+                # of all the chroma frames in string form, and two other 
+                # key-pair values for the height and width dimensions.
+                # This way you don't have to fumble with passing multiple 
+                # arguments with the map function.
+                chroma_obj =
+                executor.map(self.size_corrector, chroma_obj)
 
 
     def size_corrector(self, width: int, height: int, image):
-        '''
+        ''' 
         A size corrector. The chroma image is expected to be smaller than the luma image.
         If so, the dimensions must be matched to the luma image prior to any other operation.
-        This function must ONLY be visited if it's confirmed they're not the same size.
-        '''
-
+        ''' 
         new_image = image.resize((width, height))
         return new_image
-
 
     def append_id(self, filename: str) -> str:
         '''
@@ -75,12 +109,14 @@ class Colorizer:
         remastered_image = Image.open(colored)
 
         # Variables to perform size consistency check
+        '''
         bw_w, bw_h = bw_image.size
         cl_w, cl_h = remastered_image.size
 
         if(bw_w - cl_w != 0) or (bw_h - cl_h != 0):
             print("Image pair are different sizes, correcting...")
             remastered_image = remastered_image.resize((bw_w, bw_h))
+        '''
 
         # Construct RGB version of both b&w image and the chroma image. Needed to
         # Seamlessly convert to hsv.
@@ -120,8 +156,6 @@ class Colorizer:
         return final
 
 
-
-
 def main ():
     '''
     Main function of the module.
@@ -138,11 +172,11 @@ def main ():
         help="The image with the chroma that you want to blend the b&w image's luma on.")
     user_input = parser.parse_args()
 
-    colorizer_instant = Colorizer()
+    colorizer_instance = Colorizer()
 
-    result = colorizer_instant.color_blend(user_input.bw_input, user_input.colored_input)
-    result.save(append_id(user_input.bw_input))
+    result = colorizer_instance.color_blend(user_input.bw_input, user_input.colored_input)
+    result.save(colorizer_instance.append_id(user_input.bw_input))
 
-# So if this .py were used as a module, main wouldn't execute.
+# If this .py were used as a module, main wouldn't execute.
 if __name__ == "__main__":
     main()
--- chroma_blend.py
+++ chroma_blend.py
@@ -18,93 +18,73 @@ import cv2
 
 class CBlend:
 
-    def __init__(self: object): # Initialize (Think: A constructor)
+    def __init__(self) -> None:  # Initialize (Think: A constructor)
         pass
 
-    def folders_manager(self: object): # No input
+    def folders_manager(self) -> None:  # No input
         '''
         Manages the folders required
         for the operations. If they're there, folders are cleaned.
         If not, the folders are created.
         '''
-        if not os.path.isdir("bw_frames"):
-            print(
-                fontc.YELLOW +
-                "Creating folder: bw_frames. This will contain the black and white frames." +
-                fontc.ENDC)
-            os.mkdir("bw_frames")
-        else:
-            print("bw_frames directory found. Clearing contents..")
-            shutil.rmtree('bw_frames/')
-            os.mkdir("bw_frames")
 
-        if not os.path.isdir("source_frames"):
-            print(
-                fontc.YELLOW +
-                "Creating folder: bw_folder. This will contain the colored source frames." +
-                fontc.ENDC)
-            os.mkdir("source_frames")
-        else:
-            print("source_frames directory found. Clearing contents..")
-            shutil.rmtree('source_frames/')
-            os.mkdir("source_frames")
+        folders = ['bw_frames', 'source_frames', 'output_frames']
 
-        if not os.path.isdir("output_frames"):
-            print(
-                fontc.YELLOW +
-                """Creating folder: output_frames.
-                This will contain the final higher-res colored frames.
-                """
-                + fontc.ENDC)
-            os.mkdir("output_frames")
-        else:
-            print("output_frames directory found. Clearing contents..")
-            shutil.rmtree('output_frames/')
-            os.mkdir("output_frames")
+        for folder in folders:
+            if not os.path.isdir(folder):
+                print(
+                    fontc.YELLOW +
+                    f"Creating folder: {folder}." +
+                    fontc.ENDC)
+                os.mkdir(folder)
+            else:
+                print(f"{folder} directory found. Clearing contents..")
+                shutil.rmtree(folder)
+                os.mkdir(folder)
 
-    def png2mp4(self: object, vidin: str) -> 'mp4': 
+
+    def png2mp4(self, vidin: str) -> None:
         '''
         Function that converts the sequence of pngs to mp4's.
         '''
+        # Get FPS of input bw video.
+        cap = cv2.VideoCapture(vidin)
+        fps = cap.get(cv2.CAP_PROP_FPS)
 
-        #Get FPS of inputted bw video.
-        cap=cv2.VideoCapture(vidin)
-        fps=cap.get(cv2.CAP_PROP_FPS)
-
-
-
-        #Preparing all variables required to write a video using the f_out frames.
-        images = [img for img in os.listdir('output_frames') if img.endswith(".png")]
+        # Preparing all variables required to write a video using the f_out
+        # frames.
+        images = [img for img in os.listdir(
+            'output_frames') if img.endswith(".png")]
         frame = cv2.imread(os.path.join('output_frames', images[0]))
         height, width, layers = frame.shape
-        video = cv2.VideoWriter('final_output.avi', cv2.VideoWriter_fourcc(*'XVID'), fps, (width, height))
+        video = cv2.VideoWriter(
+            'final_output.avi', cv2.VideoWriter_fourcc(
+                *'XVID'), fps, (width, height))
 
-
-        #The list is initially unsorted, this is to fix that.
+        # The list is initially unsorted, this is to fix that.
         images = sorted(images, key=lambda x: int(x[0:-6]))
 
         print("Writing video. Please wait..")
 
         for image in images:
-
             video.write(cv2.imread(os.path.join('output_frames', image)))
 
         cv2.destroyAllWindows()
         video.release()
 
 
-def main ():
+def main():
     '''
     Main function of chroma_blend.py
     Manages provded arguments, runs folder checks, calls other modules.
     '''
-
     # First objective of main is to collected provided arguments.
-    # It'll hold them, initalize a chroma_blend object, and then give it the held arguments.
+    # It'll hold them, initalize a chroma_blend object, and then give it the
+    # held arguments.
 
-    source_frames_count = 0
-    bw_frames_count = 0
-    loop_len_count = 0
+    source_frames_count: int = 0
+    bw_frames_count: int = 0
+    loop_len_count: int = 0
 
     print(fontc.OKBLUE + "Cblend activated." + fontc.ENDC)
 
@@ -117,19 +97,16 @@ def main ():
         help="The .mp4 with the chroma that you want to blend the b&w mp4's luma on.")
     user_input = parser.parse_args()
 
-
     cblend_instance = CBlend()
 
     # Check if all directories are in place.
-    # If they're not there, they're created
-    # If they're already there, clear them.
     cblend_instance.folders_manager()
 
-
     v2p_instance = Vid2PNGs()
-    source_frames_count = v2p_instance.frame_extract(user_input.colored_vid_input, "source_frames")
-    bw_frames_count = v2p_instance.frame_extract(user_input.bw_vid_input, "bw_frames")
-
+    source_frames_count: int = v2p_instance.frame_extract(
+        user_input.colored_vid_input, "source_frames") or 0
+    bw_frames_count: int = v2p_instance.frame_extract(
+        user_input.bw_vid_input, "bw_frames") or 0
 
     print(
         fontc.CYAN +
@@ -150,27 +127,34 @@ def main ():
     else:
         loop_len_count = source_frames_count
 
-    if source_frames_count < bw_frames_count:
-        loop_len_count = source_frames_count
-    elif bw_frames_count < source_frames_count:
-        loop_len_count = bw_frames_count
+    # The loop length count will run up to the last frame of either the bw vid or the colored vid.
+    # This is because we have no guarantee that both vids will have identical
+    # number of frames.
+    loop_len_count = source_frames_count if (
+        source_frames_count < bw_frames_count) else bw_frames_count
 
-
+    frame_names: List[tuple] = []
+    
     for counter in range(loop_len_count + 1):
+        num = str(counter)
+        frame_name_set = (f'bw_frames/' + num + ".png",
+            f'source_frames/' + num + "_c.png",
+            f'output_frames/' + num + "_f.png"),
+        frame_names.append(frame_name_set)
 
-        bw_name = "bw_frames/" + str(counter) + ".png"
-        cl_name = "source_frames/" + str(counter) + "_c.png"
-        final_name = "output_frames/" + str(counter) + "_f.png"
-        #print("File names are: " + bw_name + " and " +  cl_name)
+    colorizer = Colorizer()
+    colorizer.size_check(frame_names)
+    
+    '''
+        for counter in range(loop_len_count + 1):
 
-        colblend_instance = Colorizer()
-        final_output = colblend_instance.color_blend(bw_name, cl_name)
-        final_output.save(final_name)
+            final_output = colorizer.color_blend(bw_name, cl_name)
+            final_output.save(final_name)
 
+        print("Creating video..")
 
-    print("Creating video..")
-
-    cblend_instance.png2mp4(user_input.bw_vid_input)
+        cblend_instance.png2mp4(user_input.bw_vid_input)
+    '''
 
     print("Cleaning up extracted frames...")
     shutil.rmtree('source_frames/')
